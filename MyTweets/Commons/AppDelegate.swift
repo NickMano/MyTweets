74d7137dd62e93cf158d81c08a7d4ed0e02eff2b
Nico <nmanograsso@gmail.com>
2021-06-14 21:37:21 -0300

    remove: sceneDelegate

---
+++ MyTweets/Commons/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  MyTweets
+//
+//  Created by Nicolas Manograsso on 08/06/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    var coordinator: MainCoordinator?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let navController = UINavigationController()
+        
+        coordinator = MainCoordinator(navigationController: navController)
+        coordinator?.start()
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
