051dfc51d5204aebab141e4556d77cef45800689
Nico <nmanograsso@gmail.com>
2021-06-21 09:58:23 -0300

    add: table view cell extension

---
+++ MyTweets/Extensions/UITableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  UITableViewCell.swift
+//  MyTweets
+//
+//  Created by Nicolas Manograsso on 21/06/2021.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var identifier: String {
+        String(describing: self)
+    }
+}
