f8e86c2802c2c5d2580bc1094fa9f51bb6dd435f
Nico <nmanograsso@gmail.com>
2021-08-04 00:17:30 -0300

    add: test cases to register form

--- MyTweetsTests/Views/Register/RegisterViewModelTest.swift
+++ MyTweetsTests/Views/Register/RegisterViewModelTest.swift
@@ -9,5 +9,57 @@ import XCTest
 @testable import MyTweets
 
 final class RegisterViewModelTest: XCTestCase {
+    func testValidForm() {
+        let sut = RegisterViewModel()
+        
+        let value = sut.isValidForm(userName: "user", password: "pass", email: "email")
+        
+        XCTAssertTrue(value.isValid)
+        XCTAssertNil(value.error)
+    }
     
+    func testEmptyForm() {
+        let sut = RegisterViewModel()
+        
+        let value = sut.isValidForm(userName: "", password: "", email: "")
+        
+        XCTAssertFalse(value.isValid)
+        XCTAssertEqual(value.error, .allFields)
+    }
+    
+    func testEmptyUserName() {
+        let sut = RegisterViewModel()
+        
+        let value = sut.isValidForm(userName: "", password: "pass", email: "email")
+        
+        XCTAssertFalse(value.isValid)
+        XCTAssertEqual(value.error, .someField)
+    }
+    
+    func testEmptyPassword() {
+        let sut = RegisterViewModel()
+        
+        let value = sut.isValidForm(userName: "user", password: "", email: "email")
+        
+        XCTAssertFalse(value.isValid)
+        XCTAssertEqual(value.error, .someField)
+    }
+    
+    func testEmptyEmail() {
+        let sut = RegisterViewModel()
+        
+        let value = sut.isValidForm(userName: "user", password: "pass", email: "")
+        
+        XCTAssertFalse(value.isValid)
+        XCTAssertEqual(value.error, .someField)
+    }
+    
+    func testErrorForm() {
+        let sut = RegisterViewModel()
+        
+        let value = sut.isValidForm(userName: nil, password: "pass", email: "")
+        
+        XCTAssertFalse(value.isValid)
+        XCTAssertEqual(value.error, .generic)
+    }
 }
